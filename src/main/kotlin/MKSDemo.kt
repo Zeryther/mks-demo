89cc1d26684497f609d989aee3571a07d7953a65
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2021-04-08 19:29:33 +0200

    Added Spigradle

---
+++ src/main/kotlin/MKSDemo.kt
@@ -0,0 +1,6 @@
+package com.gigadrivegroup.mksdemo
+
+import kr.entree.spigradle.annotations.SpigotPlugin
+import org.bukkit.plugin.java.JavaPlugin
+
+@SpigotPlugin public class MKSDemo : JavaPlugin()
